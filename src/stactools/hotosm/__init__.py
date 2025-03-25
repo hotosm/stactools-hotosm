9efb3083fef36670e6a3033de92a2a6bd5d31cdb
Chris Holden <ceholden@gmail.com>
2025-03-25 17:16:02 -0400

    fix: copy ruff lint rules & update code to follow

--- src/stactools/hotosm/__init__.py
+++ src/stactools/hotosm/__init__.py
@@ -1,1 +1,3 @@
-from .__version__ import __version__
+"""STAC creation tools for HOTOSM OpenAerialMap data."""
+
+from .__version__ import __version__  # noqa: F401
--- tests/test_package.py
+++ tests/test_package.py
@@ -1,8 +1,13 @@
+"""Package and package distribution tests."""
+
 import importlib.metadata
 
 
 def test_package_import_version():
-    # https://packaging.python.org/en/latest/discussions/single-source-version/
+    """Ensure our package imports and the version matches distributed metadata.
+
+    https://packaging.python.org/en/latest/discussions/single-source-version/
+    """
     from stactools.hotosm import __version__
 
     assert importlib.metadata.version("stactools.hotosm") == __version__
