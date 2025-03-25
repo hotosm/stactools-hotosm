db893c153f18de9fada5a93d2283fdf5567f6ff0
Chris Holden <ceholden@gmail.com>
2025-03-25 16:50:40 -0400

    use __version__.py per similar to other hotosm repos

--- tests/test_package.py
+++ tests/test_package.py
@@ -1,4 +1,8 @@
-def test_package_imports():
+import importlib.metadata
+
+
+def test_package_import_version():
+    # https://packaging.python.org/en/latest/discussions/single-source-version/
     from stactools.hotosm import __version__
 
-    print(f"Package version is {__version__}")
+    assert importlib.metadata.version("stactools.hotosm") == __version__
