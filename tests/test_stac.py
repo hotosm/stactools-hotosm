0e40b0195b4ee19479b58ab7dcc9caa528951ad2
Chris Holden <ceholden@gmail.com>
2025-04-29 11:34:48 -0400

    test: test stac extension from package

--- tests/test_stac.py
+++ tests/test_stac.py
@@ -3,11 +3,8 @@
 import datetime as dt
 from unittest.mock import patch
 
-import numpy as np
 import pytest
-import rasterio
 from pystac.utils import str_to_datetime
-from rasterio.transform import from_bounds
 
 from stactools.hotosm.constants import COLLECTION_ID
 from stactools.hotosm.exceptions import AssetNotFoundError
@@ -21,40 +18,9 @@ def test_create_collection():
     collection.validate()
 
 
-@pytest.fixture
-def example_oam_image(example_oam_metadata: OamMetadata, tmp_path) -> OamMetadata:
-    """Provision data for the example OAM metadata item."""
-    data = np.random.randint(low=0, high=255, size=(3, 13, 42), dtype="uint8")
-    profile = {
-        "count": data.shape[0],
-        "height": data.shape[1],
-        "width": data.shape[2],
-        "dtype": "uint8",
-        "crs": rasterio.crs.CRS.from_string(example_oam_metadata.projection_wkt),
-        "transform": from_bounds(*example_oam_metadata.bbox, *data.shape[1:]),
-    }
-
-    example_oam_metadata.image_url = str(tmp_path / example_oam_metadata.image_url)
-    with rasterio.open(
-        example_oam_metadata.image_url, "w", driver="COG", **profile
-    ) as dst:
-        dst.write(data)
-
-    example_oam_metadata.thumbnail_url = str(
-        tmp_path / example_oam_metadata.thumbnail_url
-    )
-    with rasterio.open(
-        example_oam_metadata.thumbnail_url, "w", driver="PNG", **profile
-    ) as dst:
-        dst.write(data)
-
-    return example_oam_metadata
-
-
 def test_create_item(example_oam_image: OamMetadata):
     """Test Item creation."""
     item = create_item(example_oam_image)
-    item.validate()
 
     assert item.collection_id == COLLECTION_ID
 
---
+++ tests/test_stac_extension.py
@@ -0,0 +1,37 @@
+"""Ensure that the STAC we create follows our STAC extension."""
+
+import json
+from pathlib import Path
+
+import pytest
+from pystac.validation import JsonSchemaSTACValidator
+
+from stactools.hotosm.oam_metadata import OamMetadata
+from stactools.hotosm.stac import OAM_EXT_SCHEMA, create_item
+
+OAM_STAC_EXTENSION_PATH = (
+    Path(__file__).parents[1].joinpath("stac-extension", "json-schema", "schema.json")
+)
+
+
+@pytest.fixture
+def oam_validator() -> JsonSchemaSTACValidator:
+    """Return a STAC validator setup to validate OAM extension from local path."""
+    validator = JsonSchemaSTACValidator()
+    with OAM_STAC_EXTENSION_PATH.open() as f:
+        oam_extension = json.load(f)
+
+    # Inject the extension into the schema cache so that we don't have to fetch it.
+    # This helps local development because the extension may not be published yet!
+    validator.schema_cache[oam_extension["$id"]] = oam_extension
+
+    return validator
+
+
+def test_oam_item_validates_stac_extension(
+    example_oam_image: OamMetadata, oam_validator: JsonSchemaSTACValidator
+):
+    """Ensure our OAM STAC Item validates against our own extension."""
+    item = create_item(example_oam_image.sanitize())
+    item.stac_extensions.append(OAM_EXT_SCHEMA)
+    item.validate(oam_validator)
